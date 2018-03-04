77edd87ae8fb9620ed841c068fc91fca044fe1fb
Ben <ben@stolovitz.com>
2018-03-04 10:44:44 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require("express");
+const speed_test = require("src/spee_test");
 
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+speed_test();
 
-// 404 errors
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!");
-})
+// const express = require("express");
 
-// other errors
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-})
+// const app = express();
+// app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// // 404 errors
+// app.use(function (req, res, next) {
+//   res.status(404).send("Sorry can't find that!");
+// })
+
+// // other errors
+// app.use(function (err, req, res, next) {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// })
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- src/speed_test.js
+++ src/speed_test.js
@@ -1,2 +1,17 @@
+const fs = require("fs");
+const util = require('util');
+
+//https://stackoverflow.com/questions/31978347/fs-writefile-in-a-promise-asynchronous-synchronous-stuff
+const fs_writeFile = util.promisify(fs.writeFile);
+
 const fast_tester = require("./testers/fast");
 
+module.exports = function() {
+	return fast_tester()
+		.then((result) => {
+			const content = JSON.stringify(result);
+
+			const fileName = `test`;
+			// fs_writeFile()
+		})
+}
